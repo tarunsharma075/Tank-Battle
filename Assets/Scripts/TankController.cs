6eb369959cc16ee4a86e46debef0b2009c74db98
Tarun <tarunkumarsharma38@gmail.com>
2025-08-30 06:24:12 +0530

    Added the movement for the Tank

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -5,14 +5,29 @@ public class TankController
 {
     private TankView tankView;
     private TankModel tankModel;
+    private Rigidbody rb;
     
 public TankController(TankView view, TankModel model)
     {
-        this.tankView = view;
+        this.tankView = GameObject.Instantiate<TankView>(view);
         this.tankModel = model;
+        rb= tankView.GetRigidbody();
         tankView.SetTank(this);
         tankModel.SetTank(this);
-        GameObject.Instantiate(tankView);
+        //GameObject.Instantiate(tankView);
     }
+
+   public void MoveTank(float movement,float movementspeed)
+    {
+        rb.velocity= tankView.transform.forward * movement * movementspeed;
+
+    }
+
+    public  void RotateTank(float rotation,float rotationSpeed)
+    {
+        Vector3 vector = new Vector3(0f, rotation * rotationSpeed , 0f);
+        Quaternion deltarotation = Quaternion.Euler(vector*Time.deltaTime);
+        rb.MoveRotation(rb.rotation * deltarotation);
+    }   
 
 }
--- Assets/Scripts/TankView.cs
+++ Assets/Scripts/TankView.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class TankView : MonoBehaviour
 { private TankController tankController;
+
+    private float movement;
+    private float rotation;
+
+    [SerializeField]
+    private Rigidbody rb;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+  
     void Update()
     {
-        
+        MoveMent();
+        if(movement!= 0)
+        {
+            tankController.MoveTank(movement, 30f);
+        }
+
+        if(rotation != 0)
+        {
+            tankController.RotateTank(rotation, 100f);
+        }
+
     }
 
     public void SetTank(TankController controller)
@@ -21,4 +37,16 @@ public class TankView : MonoBehaviour
         tankController = controller;
     }
 
+    private void MoveMent()
+    {
+        movement = Input.GetAxis("Vertical");
+        rotation= Input.GetAxis("Horizontal");
+    }
+
+    public Rigidbody GetRigidbody()
+    {
+        return rb;
+    }
+
 }
+
