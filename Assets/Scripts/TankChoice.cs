0a60ecdc359271886ce0760b419fa28a351d6bec
Tarun <tarunkumarsharma38@gmail.com>
2025-09-11 14:24:19 +0530

    CreateUi

---
+++ Assets/Scripts/TankChoice.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankChoice:MonoBehaviour
+{
+    [SerializeField]
+    private TankSpawner tankspawner;
+    public void ChooseGreenTank()
+    {
+
+        tankspawner.CreateTank(TankTypes.GreenTank);
+        this.gameObject.SetActive(false);
+
+    }
+    public void ChooseBlueTank()
+    {
+        tankspawner.CreateTank(TankTypes.BlueTank);
+        this.gameObject.SetActive(false);
+
+    }
+    public void ChooseRedTank()
+    {
+        tankspawner.CreateTank(TankTypes.RedTank);
+        this.gameObject.SetActive(false);
+
+    }
+
+}
+
+
--- Assets/Scripts/TankSpawner.cs
+++ Assets/Scripts/TankSpawner.cs
@@ -22,17 +22,43 @@ public class TankSpawner : MonoBehaviour
 
     void Start()
     {
-       CreateTank();
+       
 
     }
 
-    public void CreateTank()
+    public void CreateTank(TankTypes tanktype)
     {
-       TankModel tankmodel= new TankModel(
-           tanks[1].movement,
-           tanks[1].rotation,
-           tanks[1].color);
-        TankController tankcontroller = new TankController(tankview, tankmodel);
+
+
+        if (tanktype == TankTypes.GreenTank)
+        {
+
+            TankModel tankmodel = new TankModel(
+                tanks[0].movement,
+                tanks[0].rotation,
+                tanks[0].color);
+            TankController tankcontroller = new TankController(tankview, tankmodel);
+        }else if(tanktype == TankTypes.BlueTank)
+        {
+
+
+            TankModel tankmodel = new TankModel(
+                tanks[1].movement,
+                tanks[1].rotation,
+                tanks[1].color);
+            TankController tankcontroller = new TankController(tankview, tankmodel);
+
+        }
+        else if(tanktype == TankTypes.RedTank)
+        {
+
+            TankModel tankmodel = new TankModel(
+                tanks[2].movement,
+                tanks[2].rotation,
+                tanks[2].color);
+            TankController tankcontroller = new TankController(tankview, tankmodel);
+
+        }
         
     }
 
