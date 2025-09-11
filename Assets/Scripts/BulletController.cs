50a551e38a4cbd6216091dd23b8aa324f63b4cdd
Tarun <tarunkumarsharma38@gmail.com>
2025-09-11 20:00:36 +0530

    Tank Started Fire

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController
+
+{
+
+    
+
+    private BulletView bulletview;
+   public  BulletController(BulletView bulletView)
+    {
+
+        this.bulletview = bulletView;
+    }   
+    
+
+    public void Fire(Rigidbody rb, Transform pos)
+    {
+      rb.velocity = pos.forward*30;
+
+    }
+}
---
+++ Assets/Scripts/BulletView.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices.ComTypes;
+using UnityEngine;
+
+public class BulletView : MonoBehaviour
+{
+    [SerializeField]
+   
+    BulletController bulletController;
+    [SerializeField]
+    GameObject bulletprefab;
+    float firerate = 0f;
+    float firecooldown = 0.5f;
+
+    void Start()
+    {
+        bulletController= new BulletController(this);
+    }
+
+    
+    void Update()
+    {
+        
+        firerate += Time.deltaTime;
+        if(Input.GetKeyDown(KeyCode.Space)&& firerate>firecooldown)
+        {
+            Debug.Log("space pressed");
+            firerate = 0f;
+            var Bullet = Instantiate(bulletprefab, this.transform.position, this.transform.rotation);
+            Rigidbody rb = Bullet.GetComponent<Rigidbody>();
+            Transform pos= Bullet.GetComponent<Transform>();
+            bulletController.Fire(rb, pos);
+            Destroy(Bullet, 5f);
+
+
+        }
+
+    }
+}
